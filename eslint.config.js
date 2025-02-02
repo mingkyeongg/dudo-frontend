46b59612b1399939d917ab8d8707900d8aeb728a
cla6shade <cla6shade@github.com>
2025-02-02 19:37:56 +0900

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {},
+    rules: {},
+  },
+]
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {css} from '@emotion/react';
+
+function App() {
+
+  return (
+    <>
+      <p css={css`
+        font-size: 13px;
+      `}>hello world
+      </p>
+    </>
+  )
+}
+
+export default App
