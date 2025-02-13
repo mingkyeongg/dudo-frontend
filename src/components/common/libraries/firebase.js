c98978e111977835b6d3bf062d8e212360c9b69d
mingkyeongg <ming0820@naver.com>
2025-02-14 03:15:48 +0900

    feat: 값 직접 넣어보기 테스트

--- src/components/common/libraries/firebase.js
+++ src/components/common/libraries/firebase.js
@@ -4,13 +4,22 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID
+  apiKey: "AIzaSyD0trkh8Ovh4L2uhUilxsFmNi8TBipKJ2Q",
+  authDomain: "dudo-97439.firebaseapp.com",
+  projectId: "dudo-97439",
+  storageBucket: "dudo-97439.firebasestorage.app",
+  messagingSenderId: "813978662562",
+  appId: "1:813978662562:web:9d0fd70c747fd1b00a49fb"
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APP_API_KEY,
+//   authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_APP_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_APP_ID
+// };
 
 console.log("config", firebaseConfig);
 
