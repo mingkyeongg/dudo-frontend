b13961659702b2bbf1c3044d2e5bae008e6d9800
mingkyeongg <ming0820@naver.com>
2025-02-14 03:04:43 +0900

    feat: 환경변수 추가

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  // 환경 변수 로드
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react({
+        jsxImportSource: "@emotion/react",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
+    define: {
+      "process.env": env,
+    },
+  };
 });
